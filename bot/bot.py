4c7e9ce211bf8def0db759160f47ebffd4000da0
karri <husnoraxamitova@gmail.com>
2025-04-10 10:29:15 +0500

    finish

--- bot/bot.py
+++ bot/bot.py
@@ -3,7 +3,7 @@ from os import getenv
 from aiogram import Bot, Dispatcher
 from handlers.handlers import router as handlers_router
 from dotenv import load_dotenv
-
+from handlers.commands import my_commands
 load_dotenv()
 TOKEN = getenv("BOT_TOKEN")
 API_KEY = getenv("API_KEY")
@@ -14,6 +14,7 @@ dp.include_router(handlers_router)
 async def main() -> None:
     bot = Bot(token=TOKEN)
     await dp.start_polling(bot)
+    await bot.set_my_commands(my_commands)
 if __name__ == "__main__":
     print("Starting bot...")
     asyncio.run(main())
--- handlers/commands.py
+++ handlers/commands.py
@@ -1,2 +1,7 @@
+from aiogram.types import BotCommand
 
+my_commands = [
+    BotCommand(command="start",description="Ishga tushirish"),
+    BotCommand(command="help",description="yozdam"),
+]
 
