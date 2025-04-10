e3b99d28b0e48376a98c04b7acf8e39d4478e3fe
karri <husnoraxamitova@gmail.com>
2025-04-10 10:16:00 +0500

    bot tugatildi

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -1,5 +1,6 @@
 from aiogram import Router, F
 from aiogram.filters import Command
+from aiogram.fsm.context import FSMContext
 from aiogram.types import Message, ReplyKeyboardMarkup
 import os
 import requests
@@ -13,12 +14,28 @@ async def command_start_handler(message: Message) -> None:
     await message.answer("Valyuta ayirboshlash botiga xush kelibsiz",reply_markup=kb_exchange)
 
 @router.message(F.text=="💲 ➡️ 🇺🇿")
-async def enter_sum(msg: Message):
+async def enter_sum(msg: Message,state: FSMContext):
+    await state.update_data(valyuta=msg.text)
     await msg.answer("Summani kiriting: ")
 
-@router.message(F.text)
-async def usd_to_sum(msg: Message):
+@router.message(F.text=="🇺🇿 ➡️ 💲")
+async def uzs_enter_summa(msg: Message,state: FSMContext):
+    await state.update_data(valyuta=msg.text)
+    await msg.answer("Summani kiriting: ")
+
+@router.message(F.text.isdigit())
+async def usd_to_sum(msg: Message,state: FSMContext):
     summa=int(msg.text)
-    kurs= await currency_exchange(_from="USD",_to="UZS")
-    await msg.answer(f"{summa}-->UZS={summa*kurs} UZS")
+    data=await state.get_data()
+    if data and data['valyuta']=='💲 ➡️ 🇺🇿':
+        kurs=await currency_exchange("USD", "UZS")
+        await msg.answer(f"{summa}-->UZS={summa * kurs} UZS")
+        await state.clear()
+    else:
+        kurs=await currency_exchange("UZS", "USD")
+        await msg.answer(f"{summa}-->USD={summa * kurs} USD")
+        await state.clear()
+
+
+
 
